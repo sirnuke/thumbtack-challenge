5033923085492e655cb3e1e4010192e2e68d441f
Bryan DeGrendel <bryan@degrendel.com>
2014-07-10 16:39:23 -0400

    Anagram now converts each string to identifiers

--- anagram.py
+++ anagram.py
@@ -6,11 +6,17 @@ ord_offset = ord('a')
 
 letter_bits = []
 
-for i in range(0, ord('z') - ord_offset):
+for i in range(0, ord('z') - ord_offset + 1):
   letter_bits.append(2**i)
 
 for line in sys.stdin:
   line = re.sub('[\W\d]+', ' ', line)
   for word in line.split():
-    print word
+    if len(word) < 4:
+      continue
+    word = word.lower()
+    id = 0
+    for c in word:
+      id |= letter_bits[ord(c) - ord_offset]
+    print "{} is {}".format(word, id)
 

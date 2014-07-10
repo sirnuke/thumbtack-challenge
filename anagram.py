bf183b5aa66a1e30ca8b27730ca440260b6648fc
Bryan DeGrendel <bryan@degrendel.com>
2014-07-10 17:19:20 -0400

    Created a function to formulate strings
    1. Hello,world ->dellhoorw or so.

--- anagram.py
+++ anagram.py
@@ -27,6 +27,14 @@ def is_duplicate(corpus, word):
       return True
   return False
 
+def create_compare_string(words):
+  compare = ""
+  for i in range(26):
+    char = chr(i + ord_offset)
+    for word in words:
+      compare += char*word["data"][i]
+  return compare
+
 for line in sys.stdin:
   line = re.sub('[\W\d]+', ' ', line)
   for s in line.split():

400fa063be5aec649dfbcf783fe090e40d854c84
Bryan DeGrendel <bryan@degrendel.com>
2014-07-10 16:24:06 -0400

    Creates translation list and regexs text

--- anagram.py
+++ anagram.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python2.7
 
-import sys
+import re, sys
+
+ord_offset = ord('a')
+
+letter_bits = []
+
+for i in range(0, ord('z') - ord_offset):
+  letter_bits.append(2**i)
 
 for line in sys.stdin:
-  print line
+  line = re.sub('[\W\d]+', ' ', line)
+  for word in line.split():
+    print word
 

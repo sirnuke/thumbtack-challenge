366d0bda59d08b29f143c02651363d4ebd0b77a2
Bryan DeGrendel <bryan@degrendel.com>
2014-07-15 00:47:03 -0400

    Rework up until the point of checking pairs
    1. Only return longest and length of Anagram are now function arguments,
       set to True and 2 by default.  Apparently the test only uses anagram
       pairs (oops).

--- anagram.py
+++ anagram.py
@@ -4,26 +4,13 @@ import itertools
 import re
 import sys
 
-_ORD_A = ord('a')
-DEFAULT_ANAGRAM_LENGTH = 2
+ANAGRAM_LENGTH = 2
+ONLY_LONGEST = True
 MINIMUM_WORD_LENGTH = 4
-
-class Pair(object):
-  def __init__(self, words):
-    self.words = words
-    self.key = 0
-    self.length = 0
-    for word in words:
-      pass
-    #  self.length += len(word)
-      #for char in word.lower():
-        #self.key |= 2**(ord(char) - _ORD_A)
 
 class Corpus(object):
   def __init__(self):
     self._words = []
-    self._composition = {}
-    self.matches = []
 
   def stdin(self):
     #for line in re.sub('[\W\d]+', ' ', sys.stdin.read()).split('\n'):
@@ -41,25 +28,16 @@ class Corpus(object):
         return
     self._words.append(word)
 
-  def find_matches(self, length):
-    for pair in itertools.permutations(self._words, length):
-      p = Pair(pair)
+  def find_matches(self, length=ANAGRAM_LENGTH, only_longest=ONLY_LONGEST):
+    matches = []
+    data = {}
+    key_length = MINIMUM_WORD_LENGTH * length
+    for pair in itertools.combinations(self._words, length):
+      t = ''.join(sorted(itertools.chain.from_iterable(pair)))
 
 
 if __name__ == '__main__':
-  anagram_length = DEFAULT_ANAGRAM_LENGTH
-  if len(sys.argv) > 2:
-    if sys.argv[1][0] == '-':
-      print "Usage: {} [anagram-length]".format(sys.argv[0])
-      print
-      print "Where anagram-length is the positive, integer length of each anagram [{}]" \
-          .format(DEFAULT_ANAGRAM_LENGTH)
-      exit()
-    anagram_length = int(sys.argv[1])
-    if anagram_length <= 0:
-      print "Invalid anagram-length of {}".format(sys.argv[1])
-      exit(1)
-
   corpus = Corpus()
   corpus.stdin()
+  corpus.find_matches()
 
